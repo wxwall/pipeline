0145acab3c5cd5f4829ba56aac4b06c72e027544
高鹏GabryGao <gaopeng@gznb.com>
2017-08-23 10:54:04 +0800

    修改异常

--- src/main/java/com/skynet/pipeline/PipelineException.java
+++ src/main/java/com/skynet/pipeline/PipelineException.java
@@ -5,7 +5,6 @@ package com.skynet.pipeline;
 
 /**
  * @author GaoPeng
- *
  */
 public class PipelineException extends Exception {
 
@@ -14,20 +13,23 @@ public class PipelineException extends Exception {
      */
     private static final long serialVersionUID = -9112421835385732878L;
 
-    public PipelineException() {
-        super();
-    }
+    private Exception targetException;
 
     public PipelineException(final String message) {
         super(message);
     }
 
-    public PipelineException(final String message, final Throwable cause) {
+    public PipelineException(final String message, final Exception cause) {
         super(message, cause);
+        targetException = cause;
     }
 
-    public PipelineException(final Throwable cause) {
+    public PipelineException(final Exception cause) {
         super(cause);
+        targetException = cause;
     }
 
+    public Exception getTargetException() {
+        return targetException;
+    }
 }
